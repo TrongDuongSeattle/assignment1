367959a1cb3a0b4bb0bdf7311cd4b157e0f141e4
Trong <you@example.com>
2024-02-22 19:12:38 -0800

    values aren't assigned to array
    working on a print function to make sure everything is order
    I had it, but it wasn't print properly
    now it's printing properly but the values aren't in the array. Where are
    they coming from???
    
    using gdb x/10 &array shows all 0's

---
+++ sortTest.s
@@ -0,0 +1,127 @@
+.data
+.balign 4
+array: 
+	.skip 40
+i: 
+	.word 0
+j: 
+	.word 0
+randInt: 
+	.word 0
+getNum: 
+	.asciz "%d\n"
+unsorted:
+	.asciz "Unsorted array: \n"
+sorted:
+	.asciz "Sorted array: \n"
+testPrompt:
+	.asciz "array[%d] =  %d\n"
+.text
+.global main
+main:
+str lr, [sp, #4]!
+//generating time seed
+ldr r0, =randInt
+ldr r1, [r0]
+bl time
+bl srand			//rand returns random number to r0
+
+//populate array with random values
+ldr r1, =array
+ldr r2, =i
+
+b test
+
+loop:
+ldr r2, =i
+ldr r3, [r2] //get value of i
+add r4, r1, r3, LSL #2
+
+bl randomNumberGenerator
+continue:
+
+ldr r5, =randInt
+ldr r6, [r5]
+
+str r6, [r4]
+add r3, r3, #1
+str r3, [r2]
+/*
+push {r0 - r3}
+ldr r0, =testPrompt
+ldr r1, =i
+ldr r1, [r1]
+ldr r2, =array
+ldr r2, [r2]
+//ldr r1, [r1] //putting this before shows offset
+add r2, r2, r3, LSL #2
+//ldr r1, [r1] //this shows value at
+bl printf
+pop {r0 - r3}
+*/
+
+test: 
+ldr r2, =i
+ldr r3, [r2]
+cmp r3, #5
+blt loop
+mov r0, #0
+b readloop
+
+
+randomNumberGenerator: 
+push {r0 - r5}
+/*
+ldr r0, =randInt
+ldr r1, [r0]
+bl time
+bl srand			//rand returns random number to r0
+*/
+ldr r0, =randInt
+ldr r1, [r0]
+bl rand
+mov r1, r0		//move to r1, the value at r0
+
+ldr r2, =randInt//load r2 with address of randInt
+str r1, [r2]	//str to register 1, the value at register 2
+
+//divide by 10
+mov r4, #99
+ldr r1, =randInt
+ldr r2, [r1]
+udiv r3, r2,r4 
+mul r5, r3, r4 
+sub r2, r2, r5
+str r2, [r1] //str the final value back to r1, randInt
+
+ldr r0, =getNum
+ldr r1, =randInt
+ldr r1, [r1]
+bl printf
+pop {r0 - r5}
+
+bl continue
+readloop:
+
+	cmp r0, #5
+	beq end
+	ldr r1, =array
+	lsl r2, r0, #2
+	add r2, r1, r2
+	ldr r1, [r2]
+	push {r0 - r2}
+	ldr r0, =testPrompt
+	mov r2, r1
+	mov r1, r0
+	bl printf
+	pop {r0 - r2}
+	add r0, r0, #1
+	b readloop
+end:
+
+ldr lr, [sp], #4
+
+.global printf
+.global rand
+.global srand
+.global time
