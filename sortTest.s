33daf89d79f92af54b01ca19b22f72122afdcc42
trong <leafylotus@gmail.com>
2024-02-25 20:51:25 -0800

    Massive Debug Session
    Code review with A.K.,
    we saw 5 outputs, there for we were only printing i
    values kept getting smaller due to incrementing the adress by 1 byte
    A.K. pointed out the decreasing numbers as an example of Endianess, as
    the bytes get moved to the left
    
    explained the pushing and popping registers as way to return
    
    went though everything line by line tracing every bit of logic. The code
    is inherently confusing and when I noticed that a few commits ago...I
    probably should've restarted.
    
    Here we go!

--- sortTest.s
+++ sortTest.s
@@ -9,7 +9,7 @@ j:
 randInt: 
 .word 0
 getNum: 
-.asciz "%d\n"
+.asciz "getNum: %d\n"
 unsorted:
 .asciz "Unsorted array: \n"
 sorted:
@@ -24,7 +24,7 @@ testSelSort:
 .ascii "in selectionSort\n"
 
 printValue:
-.asciz "%d "
+.asciz "printValue: %d "
 .text
 .global main
 main:
@@ -36,20 +36,21 @@ bl time
 bl srand			//rand returns random number to r0
 
 //populate array with random values
-ldr r1, =array
-ldr r2, =i
+ldr r1, =array		//loading adress of array, r1 contains address of array
+ldr r2, =i			//loading address of i, r2 contains address of i
 
+//print unsorted array
 push {r0 - r2}
 ldr r0, =unsorted
 bl printf
-pop {r0 -r2}
-b test
+pop {r0 -r2}		//assuming it pops' correctly
+b test				//begin outer loop, i
 
 loop:
 ldr r2, =i
 ldr r3, [r2] //get value of i
 add r4, r1, r3, lsl #2
-bl randomNumberGenerator
+b randomNumberGenerator
 
 continue:
 ldr r5, =randInt
@@ -59,11 +60,11 @@ add r3, r3, #1
 str r3, [r2]
 
 test: 
-ldr r2, =i
-ldr r3, [r2]
-cmp r3, #5
-blt loop
-mov r4, #0
+ldr r2, =i		//loading adresss of i, r2 contians of address of i
+ldr r3, [r2]	//loading value at i, r3 == i
+cmp r3, #5		
+blt loop		//r3 < 5? ie i < 5?
+mov r4, #0		//r4 == minimum
 b readloop
 
 randomNumberGenerator: 
@@ -84,9 +85,10 @@ ldr r2, [r1]
 udiv r3, r2,r4 
 mul r5, r3, r4 
 sub r2, r2, r5
+// todo: add 1 here
 str r2, [r1] //str the final value back to r1, randInt
 pop {r0 - r5}
-bl continue
+b continue
 
 readloop:
 cmp r4, #5		//using r4 as incrementor
@@ -110,14 +112,19 @@ pop {r0 - r3}
 
 push {r0 - r3}
 
+// initilize i to 0
 ldr r0, =i		
 ldr r1, [r0]
 mov r1, #0
 str r1, [r0]
+
+// initilize j to 0
 ldr r0, =j
 ldr r1, [r0]
 mov r1, #0
 str r1, [r0]	//reset i and j to 0
+
+//init minimum to 0
 mov r4, #0		//minimum, should i make this global?
 b selectionTest		// branch to test
 
@@ -126,11 +133,18 @@ ldr r2, =j
 ldr r3, [r2]
 mov r4, r3		//minimum = j
 
+push {r0 - r3}
+ldr r0, =testJ
+bl printf
+pop {r0 - r3}
+
 //incrementing j		
 add r3, r3, #1
 str r3, [r2]	//
 
-cmp r3, #5		//compare i and j, 
+//b return
+
+cmp r3, #5		//
 blt innerbody 
 
 b	increment_i
@@ -162,22 +176,32 @@ lsl r6, r3, #2	//r6 offset
 add r6, r5, r6
 //str	r3, [r6]    //I JUST WANT ARRAY [J]
 
-ldr r7, =array
-add r7, r7, r4		//hoping this means array[minimum]
-
 push {r0 - r3}
 ldr r0, =getNum 
-ldr r1, [r7]
+
+ldr r3, =array
+lsl r2, r4, #2
+add r3, r3, r2		//hoping this means array[minimum index]
+ldr r1, [r3]
+add r1, #100
 bl printf
 pop {r0 - r3}
 
-cmp r1, #5		//compare i and 5, 
+cmp r3, #5		//compare i and 5, 
 blt innerbody 
+b increment_i 
 
 selectionTest: 
 ldr r0, =i
 ldr r1, [r0]	//load into r1 value of i from r0
-mov r4, r1		//minimum = j
+mov r4, r1		//minimum = i
+
+// end cond
+push {r0 - r3}
+ldr r0, =printValue
+bl printf
+pop {r0 - r3}
+
 cmp r1, #5		//cmp to 5
 blt body		// branch if less than to body
 pop {r0 - r3}
@@ -191,8 +215,8 @@ ldr r0, =sorted
 bl printf
 b selectionSort
 return:
-ldr r0,=getNum
-bl scanf
+//ldr r0,=getNum
+//bl scanf
 
 ldr lr, [sp], #4
 bx lr
